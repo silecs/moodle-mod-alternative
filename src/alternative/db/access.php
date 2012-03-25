949e2df96f89af99aba9e429e8136cbb7ba9bef9
François Gannaz <francois.gannaz@silecs.info>
2012-03-25 20:09:56 +0200

    add new capacities

--- src/alternative/db/access.php
+++ src/alternative/db/access.php
@@ -1,13 +1,13 @@
 <?php
 
-// This file is part of Moodle - http://moodle.org/
+// This file is part of the module "alternative" for Moodle.
 //
-// Moodle is free software: you can redistribute it and/or modify
+// This module is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
+// This module is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
@@ -25,52 +25,42 @@
  * The system has four possible values for a capability:
  * CAP_ALLOW, CAP_PREVENT, CAP_PROHIBIT, and inherit (not set).
  *
- * It is important that capability names are unique. The naming convention
- * for capabilities that are specific to modules and blocks is as follows:
- *   [mod/block]/<plugin_name>:<capabilityname>
- *
- * component_name should be the same as the directory name of the mod or block.
- *
- * Core moodle capabilities are defined thus:
- *    moodle/<capabilityclass>:<capabilityname>
- *
- * Examples: mod/forum:viewpost
- *           block/recent_activity:view
- *           moodle/site:deleteuser
- *
- * The variable name for the capability definitions array is $capabilities
- *
  * @package    mod
  * @subpackage alternative
- * @copyright  2011 Your Name
+ * @copyright  2012 Silecs
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
-
-/***************************** remove these comment marks and modify the code as needed
-    'mod/alternative:view' => array(
-        'captype' => 'read',
-        'contextlevel' => CONTEXT_MODULE,
-        'legacy' => array(
-            'guest' => CAP_ALLOW,
-            'student' => CAP_ALLOW,
-            'teacher' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW,
-            'admin' => CAP_ALLOW
-        )
-    ),
-
-    'mod/alternative:submit' => array(
-        'riskbitmask' => RISK_SPAM,
+    'mod/alternative:choose' => array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_MODULE,
         'legacy' => array(
-            'student' => CAP_ALLOW
+            'guest' => CAP_PROHIBIT,
+            'student' => CAP_ALLOW,
         )
     ),
-******************************/
+    'mod/alternative:viewregistrations' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'legacy' => array(
+            'guest' => CAP_PROHIBIT,
+            'student' => CAP_PREVENT,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+        )
+    ),
+    'mod/alternative:forceregistrations' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'legacy' => array(
+            'guest' => CAP_PROHIBIT,
+            'student' => CAP_PROHIBIT,
+            'editingteacher' => CAP_PREVENT,
+            'editingteacher' => CAP_ALLOW,
+        )
+    ),
 );
 
