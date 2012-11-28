36735b0e6b1585bf24c7f8db443276a026c6bd77
François Gannaz <francois.gannaz@silecs.info>
2012-11-28 14:20:18 +0100

    Added backup/restore

--- src/alternative/lib.php
+++ src/alternative/lib.php
@@ -51,7 +51,7 @@ function alternative_supports($feature) {
         case FEATURE_COMPLETION_HAS_RULES:    return false;
         case FEATURE_GRADE_HAS_GRADE:         return false;
         case FEATURE_GRADE_OUTCOMES:          return false;
-        case FEATURE_BACKUP_MOODLE2:          return false;
+        case FEATURE_BACKUP_MOODLE2:          return true;
         case FEATURE_SHOW_DESCRIPTION:        return true;
         default:                        return null;
     }
