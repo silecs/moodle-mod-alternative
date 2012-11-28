17af18163564d251a0faf3603208cae938cdf553
François Gannaz <francois.gannaz@silecs.info>
2012-11-28 14:53:40 +0100

    M#1331 Enable group/groupings

--- lib.php
+++ lib.php
@@ -43,9 +43,9 @@ defined('MOODLE_INTERNAL') || die();
  */
 function alternative_supports($feature) {
     switch($feature) {
-        case FEATURE_GROUPS:                  return false;
-        case FEATURE_GROUPINGS:               return false;
-        case FEATURE_GROUPMEMBERSONLY:        return false;
+        case FEATURE_GROUPS:                  return true;
+        case FEATURE_GROUPINGS:               return true;
+        case FEATURE_GROUPMEMBERSONLY:        return true;
         case FEATURE_MOD_INTRO:               return true;
         case FEATURE_COMPLETION_TRACKS_VIEWS: return false;
         case FEATURE_COMPLETION_HAS_RULES:    return false;
