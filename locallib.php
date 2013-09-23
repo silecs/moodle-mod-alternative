32a332fea464023c4e0f8403ab3146e525e15bbb
Eric Villard <dev@eviweb.fr>
2013-09-23 11:28:56 +0200

    fix non unique option names
    Signed-off-by:Eric Villard <dev@eviweb.fr>

--- locallib.php
+++ locallib.php
@@ -260,7 +260,7 @@ function alternative_table_synth_options($alternative, $cmid) {
  */
 function alternative_table_registrations($alternative) {
     global $DB;
-    $sql = "SELECT ao.name, ao.placesavail, ao.teamplacesavail, "
+    $sql = "SELECT ao.id, ao.name, ao.placesavail, ao.teamplacesavail, "
          . "GROUP_CONCAT(CONCAT(u.firstname, ' ',u.lastname) SEPARATOR ', ') AS regusers, COUNT(u.id) AS regs, COUNT(DISTINCT ar.teamleaderid) AS teams "
          . "FROM {alternative_option} AS ao "
          . "LEFT JOIN {alternative_registration} AS ar ON (ar.optionid = ao.id) "
