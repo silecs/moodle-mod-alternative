f8b385d7ff067fc159fa43bab7196b0c553ae027
Eric Villard <dev@eviweb.fr>
2013-10-03 10:54:39 +0200

    update script header author information
    Signed-off-by:Eric Villard <dev@eviweb.fr>

--- groups.php
+++ groups.php
@@ -21,6 +21,7 @@
  *
  * @package    mod
  * @subpackage alternative
+ * @author     Eric VILLARD <dev@eviweb.fr>
  * @copyright  2012 Silecs http://www.silecs.info/societe
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
